dd68c309807f9e6593b202cc304ea12ab3d8d0ed
GeraldFZ <fanzhe.gerald@gmail.com>
2023-08-09 18:35:25 +0200

    First commit

--- test.py
+++ test.py
@@ -1,35 +1,30 @@
-import nltk
 import os
+from langdetect import detect
 
-def is_english(text):
-    # 使用NLTK的句子分割器检测文本是否为英文
-    try:
-        sentences = nltk.sent_tokenize(text)
-        for sentence in sentences:
-            words = nltk.word_tokenize(sentence)
-            for word in words:
-                if not word.isalpha():
-                    return False
-    except LookupError:
-        nltk.download('punkt')
-    return True
 
-def num_english_files(files_path):
+# 遍历文件夹中的所有txt文件
+def filter_english_txt_files(folder_path):
+    english_txt_files = []
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".txt"):
+            file_path = os.path.join(folder_path, file_name)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                file_content = file.read()
+                if detect(file_content) == "en":
+                    english_txt_files.append(file_path)
+    return english_txt_files
+
+
+if __name__ == "__main__":
+    folder_path = "your_folder_path_here"  # 替换为你的文件夹路径
+    english_txt_files = filter_english_txt_files('/Users/fanzhe/Desktop/master_thesis/Data/kialo_debatetree_data/discussions')
     num = 0
-    with open(files_path, 'r', encoding='utf-8') as file:
-        text = file.read()
-    if is_english(text):
-        num += 1
-    return num
+    if len(english_txt_files) > 0:
+        print("英文文本文件列表：")
+        for file_path in english_txt_files:
+            print(file_path)
+            num = num + 1
 
-folder_path = '/Users/fanzhe/Desktop/master_thesis/Data/kialo_debatetree_data/testsample'
-txtfiles = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
-
-num_english = 0
-for file_name in txtfiles:
-    file_path = os.path.join(folder_path, file_name)
-    print(file_path)
-    num_english += num_english_files(file_path)
-    print(file_path,num_english_files(file_path))
-
-print("Number of English files:", num_english)
+            print(num)
+    else:
+        print("未找到英文文本文件。")
