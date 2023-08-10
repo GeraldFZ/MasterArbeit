49e9f5f90fc71ad1632ceefd29fbf88a44371c3b
GeraldFZ <fanzhe.gerald@gmail.com>
2023-08-10 02:17:23 +0200

    First commit

--- test.py
+++ test.py
@@ -1,30 +1,34 @@
 import os
-from langdetect import detect
+import json
 
+def process_argument(line):
+    # 将一行文本拆分成index和content，例如：'130. Virtual/Augmented Reality Technology should replace ...'
+    index, content = line.split('. ', 1)
+    index = int(index)
+    content = content.strip()
+    return {"index": index, "content": content}
 
-# 遍历文件夹中的所有txt文件
-def filter_english_txt_files(folder_path):
-    english_txt_files = []
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith(".txt"):
-            file_path = os.path.join(folder_path, file_name)
-            with open(file_path, 'r', encoding='utf-8') as file:
-                file_content = file.read()
-                if detect(file_content) == "en":
-                    english_txt_files.append(file_path)
-    return english_txt_files
+def convert_text_to_json(input_file_path, output_file_path):
+    debate = {"debate_id": os.path.basename(input_file_path), "arguments": []}
+    with open(input_file_path, 'r', encoding='utf-8') as input_file:
+        for line in input_file:
+            argument = process_argument(line)
+            debate["arguments"].append(argument)
 
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        json.dump(debate, output_file, indent=4)
+
+def convert_folder_to_json(input_folder_path, output_folder_path):
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+
+    for file_name in os.listdir(input_folder_path):
+        input_file_path = os.path.join(input_folder_path, file_name)
+        output_file_path = os.path.join(output_folder_path, f"{os.path.splitext(file_name)[0]}.json")
+        convert_text_to_json(input_file_path, output_file_path)
 
 if __name__ == "__main__":
-    folder_path = "your_folder_path_here"  # 替换为你的文件夹路径
-    english_txt_files = filter_english_txt_files('/Users/fanzhe/Desktop/master_thesis/Data/kialo_debatetree_data/discussions')
-    num = 0
-    if len(english_txt_files) > 0:
-        print("英文文本文件列表：")
-        for file_path in english_txt_files:
-            print(file_path)
-            num = num + 1
+    input_folder_path = "input_folder"  # 替换为输入文件夹路径
+    output_folder_path = "output_folder"  # 替换为输出文件夹路径
 
-            print(num)
-    else:
-        print("未找到英文文本文件。")
+    convert_folder_to_json(input_folder_path, output_folder_path)
