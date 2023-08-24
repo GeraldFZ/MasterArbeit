ab65091cb5aa3ce7cb2af3e9a892872a7b2dafc9
GeraldFZ <fanzhe.gerald@gmail.com>
2023-08-24 02:54:30 +0200

    First commit

--- debate_class.py
+++ debate_class.py
@@ -7,16 +7,40 @@ class Debate:
         self.arguments = []
 
     class Argument:
-        def __init__(self, index, content, relative_polarity, absolute_polarity):
+        def __init__(self, index, content, relative_polarity,relative_polarity_value ):
             self.index = index
             self.content = content
             self.relative_polarity = relative_polarity
-            self.absolute_polarity = absolute_polarity
+            self.relative_polarity_value = relative_polarity_value
+            # self.absolute_polarity = absolute_polarity
+            # self.absolute_polarity_value = absolute_polarity_value
+        def absolute_polarity_compute(self, index, relative_polarity_value):
+            list_index = list(index)
+            result = relative_polarity_value
+            if len(index)>=2:
+                for argument in Debate.arguments:
+                    if argument[index] == str(list_index[:len(list_index) - 2]):
+                        result = argument.relative_polarity_value * result
+                        len(index) -1
 
-    def add_argument(self, index, content, relative_polarity, absolute_polarity):
-        argument = self.Argument(index, content, relative_polarity, absolute_polarity)
+            absolute_polarity = result
+            return absolute_polarity
+
+
+
+
+
+
+
+
+    def add_argument(self,index, content, relative_polarity, relative_polarity_value):
+        # if relative_polarity == "pro":
+        #     relative_polarity_value = 1
+        # elif relative_polarity == "con":
+        #     relative_polarity_value = -1
+        argument = self.Argument(index, content, relative_polarity, relative_polarity_value)
+
         self.arguments.append(argument)
-
 
     def load_debate_from_txt(self, file_path):
         with open(file_path, "r", encoding='utf-8') as file:
@@ -24,19 +48,27 @@ class Debate:
 
 
             for index_of_argument_debate, line in enumerate(lines):
-                # match = re.match(r'(?m)^\b(?<!\d)(\d+(\.\d+)*)\b(?=\.\s|$)', line)
-                match = re.match(r'(?m)^\b(?<!\d)(\d+(\.\d+)*)\.\s', line)
 
-                if match:
-                    if index_of_argument_debate >= 1:
-                        index = match.group(0)
-                        text_content = line.replace(match.group(0), '').strip()
-                        relative_polarity= text_content[:3]
-                        content = text_content[4:].strip()
-                        absolute_polarity = -1
-                        self.add_argument(index, content, relative_polarity, absolute_polarity)
-                    if index_of_argument_debate == 0:
-                        self.debate_topic = line.replace(match.group(0), '').strip()
+                match = re.match(r'(?m)^\b(?<!\d)(\d+(\.\d+)*)\.\s', line)
+                if index_of_argument_debate == 0:
+                    self.debate_topic = line.replace(match.group(0), '').strip()
+                    index = match.group(0)
+                    text_content = line.replace(match.group(0), '').strip()
+                    relative_polarity = "Pro"
+                    content = text_content.strip()
+                    relative_polarity_value = 1 if relative_polarity == "Pro" else -1
+
+
+                    self.add_argument(index, content, relative_polarity, relative_polarity_value)
+                elif match and index_of_argument_debate != 0:
+                    # argument.relative_polarity_value = None
+                    index = match.group(0)
+                    text_content = line.replace(match.group(0), '').strip()
+                    relative_polarity = text_content[:3].strip()
+                    relative_polarity_value = 1 if relative_polarity == "Pro" else -1
+                    content = text_content[4:].strip()
+                    self.add_argument(index, content, relative_polarity, relative_polarity_value)
+
 
 
 
@@ -60,5 +92,6 @@ if __name__ == "__main__":
     for debate in debates:
         print("topic:", debate.debate_topic)
         for argument in debate.arguments:
-            print("index:", argument.index,"content:", argument.content, "relative polarity:", argument.relative_polarity)
+            print(argument)
+            print("index:", argument.index,"content:", argument.content, "relative polarity:", argument.relative_polarity, "relative polarity value:", argument.relative_polarity_value)
 
--- test.py
+++ test.py
@@ -1,34 +1,53 @@
-import os
-import json
+Arguments = [
+    {"index": "2.", "relative_polarity_value": 1},
+    {"index": "2.3.", "relative_polarity_value": 1},
+    {"index": "3.", "relative_polarity_value": 1},
+    {"index": "2.3.2.", "relative_polarity_value": -1}
+]
 
-def process_argument(line):
-    # 将一行文本拆分成index和content，例如：'130. Virtual/Augmented Reality Technology should replace ...'
-    index, content = line.split('. ', 1)
-    index = int(index)
-    content = content.strip()
-    return {"index": index, "content": content}
+#
+# def absolute_polarity_compute(index, relative_polarity_value):
+#     # list_index = index.split(".")  # Split the index into a list of parts
+#
+#     return absolute_polarity
 
-def convert_text_to_json(input_file_path, output_file_path):
-    debate = {"debate_id": os.path.basename(input_file_path), "arguments": []}
-    with open(input_file_path, 'r', encoding='utf-8') as input_file:
-        for line in input_file:
-            argument = process_argument(line)
-            debate["arguments"].append(argument)
 
-    with open(output_file_path, 'w', encoding='utf-8') as output_file:
-        json.dump(debate, output_file, indent=4)
+if __name__ == '__main__':
+    for argument in Arguments:
+        index = argument["index"]
+        relative_polarity_value = argument["relative_polarity_value"]
+        print(index, relative_polarity_value)
+        list_index = list(index)
+        # print(list_index)
 
-def convert_folder_to_json(input_folder_path, output_folder_path):
-    if not os.path.exists(output_folder_path):
-        os.makedirs(output_folder_path)
+        result = relative_polarity_value
 
-    for file_name in os.listdir(input_folder_path):
-        input_file_path = os.path.join(input_folder_path, file_name)
-        output_file_path = os.path.join(output_folder_path, f"{os.path.splitext(file_name)[0]}.json")
-        convert_text_to_json(input_file_path, output_file_path)
+        if list_index.count(".") != 1:
+            print(list_index.count(".") != 1)
+            for argument in Arguments:
+                print(argument["index"], "".join(list_index[:-2]))
+                # 将下边的判断条件右边改成找到上一个"."的位置而不是简单地减二
+                if argument["index"] == "".join(list_index[:-2]):
+                    result = argument["relative_polarity_value"] * result
+                    list_index = "".join(list_index[:-2])
+                    # print(list_index)
+            absolute_polarity = result
+        else:
+            absolute_polarity = relative_polarity_value
 
-if __name__ == "__main__":
-    input_folder_path = "input_folder"  # 替换为输入文件夹路径
-    output_folder_path = "output_folder"  # 替换为输出文件夹路径
 
-    convert_folder_to_json(input_folder_path, output_folder_path)
+        print("absolute_polarity=", absolute_polarity)
+
+        # absolute_polarity_compute(index, relative_polarity_value)
+        # print("absolute_polarity=", absolute_polarity_compute(index, relative_polarity_value))
+
+# def reverse_index_numbering(lst):
+#     dot_positions = [i for i, char in enumerate(lst) if char == "."]
+#     dot_positions.reverse()
+#     numbered_positions = {pos: i+1 for i, pos in enumerate(dot_positions)}
+#     return numbered_positions
+#
+# # 测试列表
+# test_list = ["a", "b", ".", "c", ".", "d", "e", ".", "f"]
+# numbered_positions = reverse_index_numbering(test_list)
+# print(numbered_positions)
